22a4fb31fd82dfd0501af2e467a3b2cfe27f81b8
Gustavo Liccioni <gustavo.liccioni@gamesys.co.uk>
2017-05-22 15:22:36 +0100

    INFRASTRUCTURE - making app modular

--- cocinero-mongo/src/test/java/com/cocinero/infrastructure/SpringIntegrationTest.java
+++ cocinero-mongo/src/test/java/com/cocinero/infrastructure/SpringIntegrationTest.java
@@ -1,36 +1,24 @@
 package com.cocinero.infrastructure;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fakemongo.Fongo;
 import com.mongodb.Mongo;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @RunWith(SpringRunner.class)
 @DataMongoTest
 @ContextConfiguration(classes = SpringIntegrationTest.TestConfig.class)
 public abstract class SpringIntegrationTest {
-
-    /*private TestContextManager contextManager;
-
-    public SpringIntegrationTest(){
-        contextManager = new TestContextManager(this.getClass());
-        try {
-            contextManager.prepareTestInstance(this);
-        } catch (Exception e) {
-            throw new IllegalStateException(e.getMessage(),e);
-        }
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-        contextManager.afterTestMethod(this, this.getClass().getMethods()[0],new RuntimeException("tearDownException"));
-    }*/
 
     @Configuration
     @ComponentScan("com.cocinero.infrastructure")
@@ -44,6 +32,23 @@ public abstract class SpringIntegrationTest {
         @Override
         public Mongo mongo() throws Exception {
             return new Fongo(getDatabaseName()).getMongo();
+        }
+
+        @Bean
+        public ValidatingMongoEventListener validatingMongoEventListener() {
+            return new ValidatingMongoEventListener(validator());
+        }
+
+        @Bean
+        public LocalValidatorFactoryBean validator() {
+            return new LocalValidatorFactoryBean();
+        }
+
+        @Bean
+        public ObjectMapper objectMapper(){
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            return objectMapper;
         }
     }
 }
---
+++ cocinero-web/src/test/java/com/cocinero/infrastructure/SpringIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.cocinero.infrastructure;
+
+import com.github.fakemongo.Fongo;
+import com.mongodb.Mongo;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+@ContextConfiguration(classes = SpringIntegrationTest.TestConfig.class)
+public abstract class SpringIntegrationTest {
+
+    @Configuration
+    @ComponentScan("com.cocinero.infrastructure")
+    public static class TestConfig extends AbstractMongoConfiguration {
+
+        @Override
+        protected String getDatabaseName() {
+            return "cocinerodb-test";
+        }
+
+        @Override
+        public Mongo mongo() throws Exception {
+            return new Fongo(getDatabaseName()).getMongo();
+        }
+    }
+}
